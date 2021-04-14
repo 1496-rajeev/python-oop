d704630c72c4d2b8fd34a6ee0510222049a3eb51
1496-rajeev <1496piku@gmail.com>
2021-04-14 16:18:31 +0530

    concept of self

---
+++ self.py
@@ -0,0 +1,15 @@
+class Employee:
+    def meth1(self):
+        self.name = "ravan"
+        age = 40
+        print("meth1 name ", self.name) # with "self" attribute available for the life of the class
+        print("meth1 age ", age) #witjout "self" attribute is valid only inside the method
+
+    def meth2(self):
+        print("called meth2")
+        print("meth1 name ", self.name)
+        print("meth1 age ", age) # error because it is define in only meth1
+
+employee = Employee()
+employee.meth1() # this is same as "Employee.meth1(employee)"
+employee.meth2()
